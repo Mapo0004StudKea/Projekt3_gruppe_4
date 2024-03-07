4a0ce935a72e2318459c3e136b62165e4b9d4f06
Martin Poulsen <mapo0004@stud.kea.dk>
2024-03-07 11:27:32 +0100

    First Commit

---
+++ src/main/java/stud/kea/dk/projekt3_gruppe_4/Projekt3Gruppe4Application.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.projekt3_gruppe_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt3Gruppe4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt3Gruppe4Application.class, args);
+	}
+
+}
---
+++ src/test/java/stud/kea/dk/projekt3_gruppe_4/Projekt3Gruppe4ApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.projekt3_gruppe_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt3Gruppe4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
