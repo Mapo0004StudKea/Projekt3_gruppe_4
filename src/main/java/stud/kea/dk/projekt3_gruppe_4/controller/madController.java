797847608e8c530d2fe0f4715515de737a1d3fbd
drummdurum <sedr0001@stud.kea.dk>
2024-03-08 19:22:56 +0100

    jeg har lavet noget

--- src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
+++ src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
@@ -1,7 +1,14 @@
 package stud.kea.dk.projekt3_gruppe_4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class madController {
+
+    @GetMapping("/StopMadSpild")
+    public String StopMadSpild(){
+        return "stopMadspild";
+    }
+
 }
