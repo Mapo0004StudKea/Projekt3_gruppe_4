17b7739e901b97149a73886fed9d78182d61f256
Martin Poulsen <mapo0004@stud.kea.dk>
2024-03-11 12:29:44 +0100

    another commit

--- src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
+++ src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class madController {
 
-    @GetMapping("/StopMadSpild")
+    @GetMapping("/")
     public String StopMadSpild(){
         return "stopMadspild";
     }
