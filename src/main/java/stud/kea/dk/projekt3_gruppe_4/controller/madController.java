6252ed64bc561af26213b96e9492a460545f4f70
ViggoBeck <vibe0002@stud.kea.dk>
2024-03-12 10:04:23 +0100

    Third commit

--- src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
+++ src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
@@ -1,8 +1,14 @@
 package stud.kea.dk.projekt3_gruppe_4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class madController {
+
+    @GetMapping("/")
+    public String stop(){
+        return "Madspild";
+    }
 
 }
