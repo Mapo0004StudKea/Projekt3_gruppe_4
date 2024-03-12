58c2c79262df9f1f3c8ad34e718761813ded6818
ViggoBeck <vibe0002@stud.kea.dk>
2024-03-12 10:23:22 +0100

    Fixing images

--- src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
+++ src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
@@ -1,6 +1,7 @@
 package stud.kea.dk.projekt3_gruppe_4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class madController {
