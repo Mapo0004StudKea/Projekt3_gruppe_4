2eb1dccf31dc56ca28d9634fc8a00453e264610a
Viggo2102 <viktor.rasmussen@hotmail.com>
2024-03-12 12:45:23 +0100

    Så er der kommet postmapping til "Virksomghed", så når man vælger den i dropdown menuen, bliver man redirected til en anden side! gg

--- src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
+++ src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/madController.java
@@ -2,11 +2,13 @@ package stud.kea.dk.projekt3_gruppe_4.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class madController {
 
-    @GetMapping("/madspild")
+    @GetMapping("/Madspild")
     public String stop(){
         return "Madspild";
     }
@@ -16,6 +18,12 @@ public class madController {
     public String StopMadSpild(){
         return "stopMadspild";
     }
+    @PostMapping("/redirect")
+
+    public String redirectToPage(@RequestParam("redirectPage") String redirectPage) {
+        return "redirect:/" + redirectPage;
+    }
+
     @GetMapping("/about")
     public String about() {
         return "About";
