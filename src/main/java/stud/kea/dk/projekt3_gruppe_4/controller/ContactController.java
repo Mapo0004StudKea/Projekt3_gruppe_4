0c36c5a6177886b8b98f567b66f266849e521aea
Martin Poulsen <mapo0004@stud.kea.dk>
2024-03-13 12:59:37 +0100

    Fjernet udnødvendig kode

--- src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/ContactController.java
+++ src/main/java/stud/kea/dk/projekt3_gruppe_4/controller/ContactController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import stud.kea.dk.projekt3_gruppe_4.model.ContactData;
 import stud.kea.dk.projekt3_gruppe_4.repository.ContactRepository;
-import stud.kea.dk.projekt3_gruppe_4.service.ContactService;
 
 import java.util.List;
 
@@ -15,8 +14,6 @@ public class ContactController {
     @Autowired
     ContactRepository contactRepository;
 
-    @Autowired
-    ContactService contactService;
     @GetMapping("/kontakt")
     public String contact(Model model) {
         List<ContactData> contactDataList = contactRepository.getAllContactData();
